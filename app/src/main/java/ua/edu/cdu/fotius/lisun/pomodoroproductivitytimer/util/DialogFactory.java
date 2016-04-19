7e18518082d798839fe8d4284523ea2180a4d299
Andrew <jydimir@gmail.com>
2016-04-19 12:26:27 +0300

    dynamic dialog creation

--- app/src/main/java/ua/edu/cdu/fotius/lisun/pomodoroproductivitytimer/util/DialogFactory.java
+++ app/src/main/java/ua/edu/cdu/fotius/lisun/pomodoroproductivitytimer/util/DialogFactory.java
@@ -20,35 +20,64 @@ package ua.edu.cdu.fotius.lisun.pomodoroproductivitytimer.util;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.NumberPicker;
-import static butterknife.ButterKnife.findById;
 
 import timber.log.Timber;
 import ua.edu.cdu.fotius.lisun.pomodoroproductivitytimer.R;
+import ua.edu.cdu.fotius.lisun.pomodoroproductivitytimer.data.model.PreferencePair;
 
 public class DialogFactory {
 
     public static final int NUMBER_PICKER_DEFAULT_MAX = 99;
-    public static final int NUMBER_PICKER_DEFAULT_MIN = 1;
+    public static final int NUMBER_PICKER_DEFAULT_MIN = 2;
 
-    public static Dialog createNumberPickerDialog(Context context, String title) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View dialogContent = inflater.inflate(R.layout.dialog_number_picker, null);
-        NumberPicker numberPicker = findById(dialogContent, R.id.number_picker);
+    public static Dialog createNumberPickerDialog(Context context, String title,
+                                                  DialogEventBus eventBus,
+                                                  String preferenceKey) {
+        NumberPicker numberPicker = new NumberPicker(context);
         numberPicker.setMaxValue(NUMBER_PICKER_DEFAULT_MAX);
         numberPicker.setMinValue(NUMBER_PICKER_DEFAULT_MIN);
         numberPicker.setWrapSelectorWheel(false);
+        FrameLayout.LayoutParams params = new FrameLayout
+                .LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+        FrameLayout frameLayout = new FrameLayout(context);
+        frameLayout.addView(numberPicker, params);
+
         return new AlertDialog.Builder(context)
                 .setTitle(title)
-                .setView(dialogContent)
+                .setView(frameLayout)
                 .setPositiveButton(context.getString(R.string.dialog_positive_button),
-                        (dialog, which) -> Timber.i("Positive click"))
+                        (dialog, which) -> eventBus.send(
+                                new PreferencePair(preferenceKey,
+                                        Integer.toString(numberPicker.getValue()))))
                 .setNegativeButton(context.getString(R.string.dialog_negative_button),
-                        (dialog, which) -> Timber.i("Negative click"))
+                        (dialog, which) -> dialog.dismiss())
                 .create();
-    };
+    }
+
+    public static Dialog createNewProjectDialog(Context context, String title,
+                                                  DialogEventBus eventBus, String initValue) {
+        FrameLayout.LayoutParams params = new FrameLayout
+                .LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, Gravity.CENTER);
+        FrameLayout frameLayout = new FrameLayout(context);
+        EditText editText = new EditText(context);
+        frameLayout.addView(editText, params);
+
+        return new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setView(frameLayout)
+                .setPositiveButton(context.getString(R.string.dialog_positive_button),
+                        (dialog, which) -> eventBus.send(editText.getText().toString()))
+                .setNegativeButton(context.getString(R.string.dialog_negative_button),
+                        (dialog, which) -> dialog.dismiss())
+                .create();
+    }
+
 }
